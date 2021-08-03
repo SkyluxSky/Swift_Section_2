a57d0f4d7550dd707c2c056745d916402ca5ff4e
SkyluxSky <skyluxsky@gmail.com>
2021-08-03 00:52:04 -0400

    Init Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Skylux on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ i am richTests/i_am_richTests.swift
@@ -0,0 +1,33 @@
+//
+//  i_am_richTests.swift
+//  i am richTests
+//
+//  Created by Skylux on 8/2/21.
+//
+
+import XCTest
+@testable import i_am_rich
+
+class i_am_richTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
